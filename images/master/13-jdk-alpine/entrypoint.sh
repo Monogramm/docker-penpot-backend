d893edb169affae0c151a70c1e10a604ba0e901c
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-07-20 12:17:43 +0200

    :art: Improve startup logs format
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/master/13-jdk-alpine/entrypoint.sh
+++ images/master/13-jdk-alpine/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 set -e
 
+log() {
+  echo "[$(date +%Y-%m-%dT%H:%M:%S%:z)] $@"
+}
+
 # ------------------------------------------------------------------------------
-echo "Initializing UXBOX Backend directories..."
+log "Initializing UXBOX Backend directories..."
 mkdir -p \
     "$(echo ${UXBOX_MEDIA_DIRECTORY} | tr -d \")" \
     "$(echo ${UXBOX_ASSETS_DIRECTORY} | tr -d \")"
@@ -11,12 +15,12 @@ mkdir -p \
 
 # TODO Find a way to only update sources if new version in source
 
-echo "Copying UXBOX Backend sources..."
+log "Copying UXBOX Backend sources..."
 rsync -rlD --delete \
     --exclude "$(echo ${UXBOX_MEDIA_DIRECTORY} | tr -d \")" \
     /usr/src/uxbox/dist/ ./
 
-echo "Copying UXBOX Backend assets..."
+log "Copying UXBOX Backend assets..."
 rsync -rlD --delete \
     /usr/src/uxbox/dist/resources/public/static "$(echo ${UXBOX_ASSETS_DIRECTORY} | tr -d \")"
 
@@ -29,5 +33,5 @@ if [ -f "$(echo ${UXBOX_COLLECTIONS_CONFIG} | tr -d \")" ]; then
 fi
 
 # ------------------------------------------------------------------------------
-echo "Starting UXBOX backend..."
+log "Starting UXBOX backend..."
 exec "$@"
