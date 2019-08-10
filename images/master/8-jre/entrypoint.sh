7ef9e314d9b32e842f48d5f7c42379135a2c0cdd
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-08-10 18:02:19 +0200

    :sparkles: Copy default media collections
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/master/8-jre/entrypoint.sh
+++ images/master/8-jre/entrypoint.sh
@@ -26,11 +26,18 @@ fi
 log "Copying UXBOX Backend sources..."
 rsync -rlD --delete \
     --exclude "$(echo ${UXBOX_MEDIA_DIRECTORY} | tr -d \")" \
-    /usr/src/uxbox/dist/ ./
+    /usr/src/uxbox/dist/ \
+    ./
 
 log "Copying UXBOX Backend assets..."
 rsync -rlD --delete \
-    /usr/src/uxbox/dist/resources/public/static "$(echo ${UXBOX_ASSETS_DIRECTORY} | tr -d \")"
+    /usr/src/uxbox/dist/resources/public/static \
+    "$(echo ${UXBOX_ASSETS_DIRECTORY} | tr -d \")"
+
+log "Copying UXBOX default media..."
+rsync -rlD \
+    /usr/src/media \
+    /srv/media
 
 # ------------------------------------------------------------------------------
 # Import (new) built-in collections if any found
